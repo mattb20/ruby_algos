e2aaa531ff49e673f479f0760ecd23682dc04b81
mattb20 <matt_burgoyne@live.co.uk>
2018-06-28 18:37:49 +0100

    reverting back to all in one method due to bug that needs fixing

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -8,17 +8,20 @@ def bubblesort(array)
     swap = false
     (array.length - 1).times do |x| ## block iterates array.length - 1 times, passing in integers
       ## from 0 to array.length - 1
-      compare_elements_and_swap(array, x, swap)
+      if array[x] > array[x+1]
+        array[x], array[x+1] =  array[x+1], array[x]
+        swap = true
+      end
       end
   end
   array
 
 end
-def compare_elements_and_swap(array, index, boolean)
-    if array[index] > array[index+1]
-      array[index], array[index+1] =  array[index+1], array[index]
-      boolean = true
-    end
-end
+# def compare_elements_and_swap(array, index, boolean)
+#     if array[index] > array[index+1]
+#       array[index], array[index+1] =  array[index+1], array[index]
+#       boolean = true
+#     end
+# end
 
 puts bubblesort([3,1,3,4,5,6,4,2])
