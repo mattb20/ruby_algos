2eda68eff44e1902c5b9409bd38d5b279f529a52
mattb20 <matt_burgoyne@live.co.uk>
2018-06-28 13:39:46 +0100

    extracted comparison of elements and boolean switch into its own method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,14 +6,19 @@ def bubblesort(array)
   swap = true
   while swap
     swap = false
-    (array.length - 1).times do |x|
-      if array[x] > array[x+1]
-        array[x], array[x+1] =  array[x+1], array[x]
-        swap = true
-      end
+    (array.length - 1).times do |x| ## block iterates array.length - 1 times, passing in integers
+      ## from 0 to array.length - 1
+      compare_elements_and_swap(array, x, swap)
       end
   end
   array
 
 end
+def compare_elements_and_swap(array, index, boolean)
+    if array[index] > array[index+1]
+      array[index], array[index+1] =  array[index+1], array[index]
+      boolean = true
+    end
+end
+
 puts bubblesort([3,1,3,4,5,6,4,2])
